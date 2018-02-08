b8fa52d0d558052d0067defb2278dc548027dd21
liam07he@gmail.com <HXH_Liam@Liams-MacBook-Pro.local>
2018-02-07 16:29:21 -0800

    using node module

---
+++ app.js
@@ -0,0 +1,7 @@
+let mathModule = require('./mathlib')();
+
+console.log(mathModule.add(1, 2));
+console.log(mathModule.multiply(1, 2));
+console.log(mathModule.square(2));
+console.log(mathModule.random(1, 3));
+
